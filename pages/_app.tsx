e8d62d65fff3a94f475816c310bba99725989596
Johan Hanses <johanhanses@gmail.com>
2022-09-30 07:50:25 +0200

    fix typing of appProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { SessionProvider } from 'next-auth/react'
+import { Session } from 'next-auth'
 
 export default function MyApp({
   Component,
   pageProps: { session, ...pageProps }
-}: AppProps) {
+}: AppProps<{
+  session: Session
+}>) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class">
