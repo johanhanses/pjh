d3325e76534e60e6aabe5d9fca9d1831a0c20cd9
Johan Hanses <johanhanses@gmail.com>
2022-10-02 08:47:31 +0200

    Change active link design

--- components/CvNav.tsx
+++ components/CvNav.tsx
@@ -8,10 +8,24 @@ export const CvNav = () => {
   return (
     <nav className="block space-x-4 text pb-12 text-xl font-bold">
       <Link href="/cv">
-        <a className={router.pathname == '/cv' ? 'underline' : ''}>CV</a>
+        <a
+          className={
+            router.pathname == '/cv'
+              ? 'text-themeOrange dark:text-themeGreen'
+              : ''
+          }
+        >
+          CV
+        </a>
       </Link>
       <Link href="/cover-letter">
-        <a className={router.pathname == '/cover-letter' ? 'underline' : ''}>
+        <a
+          className={
+            router.pathname == '/cover-letter'
+              ? 'text-themeOrange dark:text-themeGreen'
+              : ''
+          }
+        >
           Cover letter
         </a>
       </Link>
